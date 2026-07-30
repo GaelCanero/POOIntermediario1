7f9efe2b2e2e56e580439de3cfb565f29727193a
gaele <gaele@CADOEVENTO2>
2026-07-30 08:35:48 -0300

    Adicionar arquivos de projeto.

---
+++ POOIntermediario1/Classes/Cliente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POOIntermediario1.Classes
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public bool Ativo { get; set; }
+        public Cliente()
+        {
+        }
+        public Cliente(int id, string nome, string email, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Telefone = telefone;
+            Ativo = true;
+        }
+        public void AtualizarContato(string telefone)
+        {
+            Telefone = telefone;
+        }
+        public void AtualizarContato(string telefone, string email)
+        {
+            Telefone = telefone;
+            Email = email;
+        }
+        public void DesativarCliente()
+        {
+            Ativo = false;
+        }
+        public void AtivarCliente()
+        {
+            Ativo = true;
+        }
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Telefone: {Telefone}");
+            if (Ativo == true)
+            {
+                Console.WriteLine($"O cliente {Nome} está ativo\n");
+            }
+            else
+            {
+                Console.WriteLine($"O cliente {Nome} está inativo.\n");
+            }
+        }
+    }
+}
---
+++ POOIntermediario1/Program.cs
@@ -0,0 +1,58 @@
+﻿using POOIntermediario1.Classes;
+
+namespace POOIntermediario1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente[] clientes = new Cliente[5];
+
+            clientes[0] = new Cliente(
+                1,
+                "Gael",
+                "gael@empresa.com",
+                "21999999999");
+
+            clientes[1] = new Cliente(
+                2,
+                "Evandro",
+                "evandro@empresa.com",
+                "21988888888");
+
+            clientes[2] = new Cliente(
+                3,
+                "Denilson",
+                "denilson@empresa.com",
+                "21977777777");
+
+            clientes[3] = new Cliente(
+                4,
+                "Vitor",
+                "vitor@empresa.com",
+                "21966666666");
+
+            clientes[4] = new Cliente(
+                5,
+                "Gustavo",
+                "gustavo@empresa.com",
+                "21955555555");
+            
+            clientes[0].AtualizarContato("21911111111");
+
+            clientes[1].AtualizarContato(
+                "21922222222",
+                "evandronovo@empresa.com");
+
+            clientes[2].DesativarCliente();
+
+            for (int i = 0; i < clientes.Length; i++)
+            {
+                if (clientes[i].Ativo)
+                {
+                    clientes[i].ExibirInformacoes();
+                }
+            }
+        }
+    }
+}
