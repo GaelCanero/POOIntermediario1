ce77251868fc76feeaecdb7c25ef10bc09c2a9b9
GaelCN <gael.canerodefigueiredo@gmail.com>
2026-07-30 13:23:47 -0300

    Update Program.cs

--- POOIntermediario1/Program.cs
+++ POOIntermediario1/Program.cs
@@ -1,4 +1,4 @@
-﻿using POOIntermediario1.Classes;
+using POOIntermediario1.Classes;
 
 namespace POOIntermediario1
 {
@@ -8,41 +8,14 @@ namespace POOIntermediario1
         {
             Cliente[] clientes = new Cliente[5];
 
-            clientes[0] = new Cliente(
-                1,
-                "Gael",
-                "gael@empresa.com",
-                "21999999999");
-
-            clientes[1] = new Cliente(
-                2,
-                "Evandro",
-                "evandro@empresa.com",
-                "21988888888");
-
-            clientes[2] = new Cliente(
-                3,
-                "Denilson",
-                "denilson@empresa.com",
-                "21977777777");
-
-            clientes[3] = new Cliente(
-                4,
-                "Vitor",
-                "vitor@empresa.com",
-                "21966666666");
-
-            clientes[4] = new Cliente(
-                5,
-                "Gustavo",
-                "gustavo@empresa.com",
-                "21955555555");
+            clientes[0] = new Cliente(1, "Gael", "gael@prevhab.com", "21999999999");
+            clientes[1] = new Cliente(2, "Evandro", "evandro@prevhab.com", "21988888888");
+            clientes[2] = new Cliente(3, "Denilson", "denilson@prevhab.com", "21977777777");
+            clientes[3] = new Cliente(4, "Vitor", "vitor@prevhab.com", "21966666666");
+            clientes[4] = new Cliente(5, "Gustavo", "gustavo@prevhab.com", "21955555555");
             
             clientes[0].AtualizarContato("21911111111");
-
-            clientes[1].AtualizarContato(
-                "21922222222",
-                "evandronovo@empresa.com");
+            clientes[1].AtualizarContato("21922222222", "evandronovo@prevhab.com");
 
             clientes[2].DesativarCliente();
 
@@ -52,6 +25,24 @@ namespace POOIntermediario1
                 {
                     clientes[i].ExibirInformacoes();
                 }
+            }
+            Console.WriteLine("Deseja mostrar os cliente inativos? Digite (S) para continuar.");
+
+            switch (Console.ReadLine().ToUpper())
+            {
+                case "S":
+                    Console.Clear();
+                    for (int i = 0; i < clientes.Length; i++)
+                    {
+                        if (!clientes[i].Ativo)
+                        {
+                            clientes[i].ExibirInformacoes();
+                        }
+                    }
+                    return;
+                    
+                default:
+                    break;
             }
         }
     }
